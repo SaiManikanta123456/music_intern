9c5235ff5a8aec40878e7e83f388f4c3eab88510
L.Sai Manikanta <113752452+SaiManikanta123456@users.noreply.github.com>
2023-06-20 17:22:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// const api_key='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';
+
+function loadAPIKey(callback) {
+    const API_KEY = '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';
+    callback(API_KEY);
+  }
